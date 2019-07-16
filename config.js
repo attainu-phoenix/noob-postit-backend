c11526106a0e1bc9628ef40793e7a0b005991599
Samarth Keskar <samarthkeskar363@gmail.com>
2019-07-16 11:45:05 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    consumerKey: 'WX8L3OrRSX71kawHfbAOgzVv8',
+    consumerSecret: 'DFYj5rUJj0pTy6l6UQaYbFFSTkIXsOvXIMM5fA9SiyYtwKk459'
+};
---
+++ index.js
@@ -0,0 +1,177 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+'use strict';
+
+var express = require('express'),
+	jwt = require('jsonwebtoken'),
+	expressJwt = require('express-jwt'),
+	router = express.Router(),
+	cors = require('cors'),
+	bodyParser = require('body-parser'),
+	request = require('request'),
+	twitter = require("twitter"),
+	config = require('./config.js');
+var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
+
+
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/postit',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'postit',
+  masterKey: process.env.MASTER_KEY || 'postit', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["SocialAccounts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
+
+var app = express();
+
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
+
+
+var corsOption = {
+	origin: true,
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	credentials: true,
+	exposedHeaders: ['x-auth-token']
+};
+app.use(cors(corsOption));
+
+// Accept JSON from the client side
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+
+// Creates a JWT
+var createToken = function(auth) {
+	return jwt.sign({
+		id: auth.id
+	}, 'my-secret',
+	{
+		expiresIn: 60 * 60 * 24 * 30
+	});
+};
+
+
+// Middleware for generating a token
+var generateToken = function (req, res, next) {
+	req.token = createToken(req.auth);
+	return next();
+};
+
+
+// Get the token from Twitter
+app.post('/auth/twitter/request', function(req, res) {
+	request.post({
+		url: 'https://api.twitter.com/oauth/request_token',
+		oauth: {
+			oauth_callback: "http%3A%2F%2Flocalhost%3A3000%2Ftwitter-callback",
+			consumer_key: config.consumerKey,
+			consumer_secret: config.consumerSecret
+		}
+	}, function (err, r, body) {
+		if (err) {
+			return res.send(500, { message: err.message });
+		}
+		var jsonStr = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+		res.send(JSON.parse(jsonStr));
+	});
+});
+
+// Signin the user and generate the token
+app.post('/auth/twitter/login', function(req, res) {
+	request.post({
+		url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
+		oauth: {
+			consumer_key: config.consumerKey,
+			consumer_secret: config.consumerSecret,
+			token: req.query.oauth_token
+		},
+		form: { oauth_verifier: req.query.oauth_verifier }
+	}, function (err, r, body) {
+		if (err) {
+			return res.send(500, { message: err.message });
+		}
+
+		const bodyString = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+		const parsedBody = JSON.parse(bodyString);
+		res.json(parsedBody);
+	});
+});
+
+
+// Creates a tweet
+app.post('/twitter/post', function(req, res) {
+	var client = new twitter({
+		consumer_key: req.body.consumer_key,
+  		consumer_secret: req.body.consumer_secret,
+  		access_token_key: req.body.access_token_key,
+  		access_token_secret: req.body.access_token_secret
+	});
+	client.post(
+		'statuses/update',
+		{status: req.body.status},
+		function(err, tweet, response) {
+			if(err) return res.status(500).end();
+			res.json(tweet)
+		}
+	);
+});
+
+
+// Deletes a tweet
+app.delete('/twitter/post/:id', function(req, res) {
+	var client = new twitter({
+		consumer_key: req.body.consumer_key,
+  		consumer_secret: req.body.consumer_secret,
+  		access_token_key: req.body.access_token_key,
+  		access_token_secret: req.body.access_token_secret
+	});
+	client.post(
+		'statuses/destroy/' + req.params.id,
+		function(err, tweet, response) {
+			console.log(err)
+			if(err) return res.status(500).end();
+			res.json(tweet)
+		}
+	);
+});
+
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+});
+
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
+});
+
+var port = process.env.PORT || 4444;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
